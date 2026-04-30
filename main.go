1503e04f398d7b5423b9608b5b038b14e1758e4e
Khalid Adisendjaja <kh411dz@gmail.com>
2026-04-30 17:10:47 +0700

    [main] update

--- main.go
+++ main.go
@@ -14,9 +14,7 @@ func main() {
 		log.Fatal("GRAVATAR_API_KEY environment variable is required")
 	}
 
-	http.HandleFunc("/gravatar/", gravatar.Handler)
-
-	addr := ":8080"
-	log.Printf("starting Gravatar proxy server on %s", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	http.HandleFunc("/profile/", gravatar.Handler)
+	addr := ":3000"
+	http.ListenAndServe(addr, nil)
 }
